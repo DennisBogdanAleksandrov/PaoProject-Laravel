8be5c5380c6ed8e36ea72b4bea7f3fbf3bece422
DennisBogdanAleksandrov <extraordinarycourtesy@gmail.com>
2019-12-21 23:14:56 -0800

    .

--- resources/js/pages/HomeP.js
+++ resources/js/pages/HomeP.js
@@ -5,40 +5,67 @@ import backgroundImg from './../../images/jfl-GfmIT_pseDQ-unsplash.jpg';
 import { CallToAction } from '../components/buttons';
 import PaoTable from '../components/PaoTable';
 import Logo from '../components/Logo';
+import styled from "@emotion/styled";
 
 
+const Title = styled.h1`
+  color: pink;
+  @media (max-width: 750px) {
+    color:blue;
+  }
+  
+`;
 
+const DivStyling = styled.div`
+  background-image: url(./../../images/jfl-GfmIT_pseDQ-unsplash.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 
-const divStyling = {
-  backgroundImage: "url(" + backgroundImg + ")",
-  backgroundPosition: '',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat'
-};
+const Button = styled.div`
+  padding: 32px;
+  background-color: pink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
 
 const Home = () => {
   return (
-    <div style={divStyling} className="h-screen"> 
-      <Navbar />
+    <DivStyling>
+      <div className="h-screen"> 
+        <Navbar />
 
-    {/* front screen */}
-      <div className="container mx-auto">
-        <div className="flex justify-center">
-          <div className="flex flex-col h-screen">
+      {/* front screen */}
+        <div className="container mx-auto">
 
-        <div className="h-32 w-32 mt-24 mb-20 mx-auto">
-          <Logo />
-        </div>
+          <Title>
+            <div>SUCCESS!</div>
+          </Title>
 
-            <CallToAction />
+          {/* <Button>success</Button> */}
+
+          <div className="flex justify-center">
+            <div className="flex flex-col h-screen">
+              
+          <div className="h-32 w-32 mt-24 mb-20 mx-auto">
+            <Logo />
+          </div>
+
+              <CallToAction />
+            </div>
+          </div>
+          <div className="flex justify-center -mt-12">
+            <PaoTable />
           </div>
         </div>
-        <div className="flex justify-center -mt-12">
-          <PaoTable />
-        </div>
+        {/* the sets */}
       </div>
-      {/* the sets */}
-    </div>
+    </DivStyling>
   );
 };
 
